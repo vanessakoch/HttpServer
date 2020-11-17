b1b7429060ab933450f9bb317d13ac6eecd3fc3b
vanessakoch <vahnkoch@gmail.com>
2020-11-17 16:53:47 -0300

    mudando inputstream para bufferedReader e outputstream para dataoutputstream

--- src/Server.java
+++ src/Server.java
@@ -2,41 +2,35 @@ import java.io.*;
 import java.net.*;
 
 public class Server implements Runnable {
-    private final int porta;
-    ServerSocket server = null;
-    
-    
-    public Server(int porta) {
-        this.porta = porta;
-    }
+	private final int port;
+	ServerSocket server = null;
+
+	public Server(int port) {
+		this.port = port;
+	}
 
 	@Override
 	public void run() {
-        try {
-            server = new ServerSocket(porta);
-            System.out.println("Servidor está rodando na porta " + porta);
+		try {
+			server = new ServerSocket(port);
+			System.out.println("Servidor está rodando na porta " + port);
 
-        	while(true) {
+			while (true) {
+				Socket client = server.accept();
+				System.out.println("Cliente conectado: " + client.getInetAddress() + " " + client.getPort() + "\n");
+				new Thread(new Worker(client)).start();
+			}
 
-	            Socket client = server.accept();
-	            
-	            System.out.println("Cliente conectado: " + client.getInetAddress() + " " + client.getPort() + "\n");
-	            
-	            new Thread(new Worker(client)).start();           
-        	}
-            
-        } catch(IOException ex) {
-            System.out.println("Servidor já está em uso.");
+		} catch (IOException ex) {
+			System.out.println("Servidor já está em uso.");
 
-            if(server != null && !server.isClosed()) {
-
-            	try {
-
-                	server.close();
-                } catch(IOException ex1) {
-                    ex1.getMessage();
-                }
-            }
-        }
+			if (server != null && !server.isClosed()) {
+				try {
+					server.close();
+				} catch (IOException ex1) {
+					ex1.getMessage();
+				}
+			}
+		}
 	}
 }
--- src/Worker.java
+++ src/Worker.java
@@ -1,92 +1,84 @@
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.InputStreamReader;
 import java.net.Socket;
 
-public class Worker implements Runnable{
+public class Worker implements Runnable {
 
 	private Socket clientSocket = null;
-    private InputStream input = null;
-    private OutputStream output = null;
-	private String request = null;
 	private String user = "admin";
 	private String password = "ifsc";
 	private String requestList[] = null;
 	private String userInput = null;
 	private String passwordInput = null;
-    
-    public Worker(Socket clientSocket) {
-        this.clientSocket = clientSocket;
-    }
 
-    public void run() {
-    	
-        try {
-			input = clientSocket.getInputStream();
-			 
-			byte[] dataByte = new byte[1024];
-			int data = input.read(dataByte);
+	public Worker(Socket clientSocket) {
+		this.clientSocket = clientSocket;
+	}
+
+	public void run() {
+
+		try {
+			BufferedReader buffer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+			StringBuilder str = new StringBuilder();
 			
-			if (data > 0){
-				request = new String(dataByte, 0 , data);
-	        	 
-				requestList = request.split("\n");
-			} else {  
-				return;
+			String method = buffer.readLine();
+			
+			while(buffer.ready()) {
+				str.append((char) buffer.read());
+				requestList = str.toString().split("\n");
 			}
-			
-			if(request.contains("POST")) {
-                System.out.println("Cliente enviou : ");
+				
+			if (method.contains("POST")) {
+				System.out.println("Cliente enviou : ");
 
-				for(String s : requestList) {
-					System.out.println(s);
-				}
-				
-				String[] listInput = requestList[requestList.length-1].split("[=&]");
-				
-				if(listInput.length == 4)
+				System.out.println(str);
+
+				String[] listInput = requestList[requestList.length - 1].split("[=&]");
+
+				if (listInput.length == 4)
 					userInput = listInput[1];
-		            passwordInput = listInput[3];
-		            
-		            response(request, userInput, passwordInput);
+					passwordInput = listInput[3];
+
+				response(userInput, passwordInput);
 			}
-            
-        } catch (IOException io) {
-        	io.printStackTrace();
-        	
-        }
-    }
+			
 
-    
-    public void response(String request, String userInput, String passwordInput) {
-    	String httpResponse = null;
-    	try {
-            output = clientSocket.getOutputStream();
+		} catch (IOException io) {
+			io.printStackTrace();
 
-            if(userInput.equalsIgnoreCase(user) && passwordInput.equalsIgnoreCase(password)) {
-            	httpResponse = 
-						"HTTP/1.1 200 OK\r\nContent-Type:text/html\r\n\r\n "
+		}
+	}
+
+	public void response(String userInput, String passwordInput) {
+		String httpResponse = null;
+
+		try {
+			DataOutputStream output = new DataOutputStream(clientSocket.getOutputStream());
+
+			if (userInput.equalsIgnoreCase(user) && passwordInput.equalsIgnoreCase(password)) {
+				httpResponse = "HTTP/1.1 200 OK\r\nContent-Type:text/html\r\n\r\n "
 						+ "<html><header>HTTP/1.1 200 OK</header> \n \n <title> Bem vindo </title> <body> Login "
 						+ "realizado com sucesso :) </body> <html>";
-				
-            } else {
-            	httpResponse = 
-						"HTTP/1.1 401 Unauthorized\r\nContent-Type:text/html\r\n\r\n "
+
+			} else {
+				httpResponse = "HTTP/1.1 401 Unauthorized\r\nContent-Type:text/html\r\n\r\n "
 						+ "<html> <header>HTTP/1.1 401 Unauthorized</header><title> Erro </title> <body> Senha ou usuario incorreto </body> <html>";
-				
-            }
-            output.write(httpResponse.getBytes("UTF-8"));
+
+			}
+			output.writeBytes(httpResponse);
+			output.writeBytes("\n");
+			System.out.println("\nServidor respondeu o cliente");
+			
 			output.flush();
 			output.close();
+			
 			clientSocket.close();
-			
-            System.out.println("\nServidor respondeu o cliente");
-            System.out.println("Conexão do cliente encerrada");
+			System.out.println("Conexão do cliente encerrada");
 
-			
-			
 		} catch (IOException i) {
 			i.printStackTrace();
 		}
-    }
+	}
 }
